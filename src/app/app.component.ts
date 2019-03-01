d2fc55aef7571101aca635864b86096252460ac1
F3L1P34479 <felipe_sf@hotmail.com>
2019-02-28 23:05:22 -0300

    alterações para aprendizado dos componentes que fazem parte do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.css']
 })
 export class AppComponent{
+  nome: string = "Felipe";
+  valor: string = "";
+  num1: string = "";
+  num2: string = "";
 
+  soma():Number{
+    this.total= Number(this.num1) + Number(this.num2);
+    return this.total;
+  }
+  apresentaNome(){
+    alert('Boa noite ' + this.valor);
+  }
+  clique(){
+    alert('boa noite');
+  }
 
 }
